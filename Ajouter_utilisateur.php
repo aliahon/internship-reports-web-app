dadda211a9a75d0a15d58e573ce48ae25c476217
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-04-20 05:04:29 +0100

    personnaliser la navbar pour chaque page

---
+++ Ajouter_utilisateur.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body data-bs-theme="dark">
+    <?php include 'include/nav.php'?>
+    <div class="container" style=" padding : 5% 10%" >
+        <?php
+            if(isset($_POST['Ajouter'])){
+                $Nom = $_POST['Nom'];
+                $Prenom = $_POST['Prenom'];
+                $Email = $_POST['Email'];
+                $mdp = $_POST['Mot_de_passe'];
+                $ID_role = $_POST['ID_role'];
+
+                if(!empty($Nom) && !empty($Prenom) && !empty($Email) && !empty($mdp) && !empty($ID_role)){
+                    require_once 'include/database.php';
+                    $sqlState = $pdo -> prepare('INSERT INTO utilisateurs VALUES(null, ?,?,?,?,?)');
+                    $sqlState -> execute([$Nom,$Prenom, $Email,$mdp, $ID_role]);
+
+                    header('location: connexion.php');
+                }
+                else { 
+                    ?>
+                    <div class="alert alert-danger" role="alert">
+                        Tous les champs sont obligatoires!
+                    </div>
+                    <?php
+                }
+            }
+
+        ?>
+        <h4 >Ajouter un Utilisateur!</h4>
+        <form class="row g-3 needs-validation" method="post" novalidate>
+            <div class="col-md-4">
+                <label for="validationCustom01" class="form-label">Nom</label>
+                <input type="text" class="form-control" id="validationCustom01" name="Nom" required>
+                <div class="valid-feedback">
+                Looks good!
+                </div>
+            </div>
+            <div class="col-md-4">
+                <label for="validationCustom02" class="form-label">Prénom</label>
+                <input type="text" class="form-control" id="validationCustom02" name="Prenom" required>
+                <div class="valid-feedback">
+                Looks good!
+                </div>
+            </div>
+            <div class="col-md-5">
+                <label for="validationCustom03" class="form-label">Email</label>
+                <input type="email" class="form-control" id="validationCustom03" name="Email" required>
+                <div class="invalid-feedback">
+                Please provide a valid Email.
+                </div>
+            </div>
+            <div class="col-md-3">
+                <label for="validationCustom04" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="validationCustom04" name="Mot de passe" required>
+                <div class="invalid-feedback">
+                Please provide a valid Password.
+                </div>
+            </div>
+            <div class="col-md-3 ">
+                <label for="validationCustom05" class="form-label">Role</label>
+                <select class="form-select" id="validationCustom05" name="ID_role" required>
+                <option value="1">Admin</option>
+                <option value="2">Chef de département</option>
+                <option value="3">Secretaire de département</option>
+                <option value="4">Etudiant</option>
+                </select>
+                <div class="invalid-feedback">
+                Please select a valid Role.
+                </div>
+            </div>
+            <div class="col-12">
+                <button class="btn btn-primary" type="submit" name="Ajouter">Ajouter Utilisateur</button>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
--- connexion.php
+++ connexion.php
@@ -8,7 +8,22 @@
 </head>
 </head>
 <body data-bs-theme="dark">
-    <?php include 'include/nav.php'?>
+<nav class="navbar  navbar-expand-lg  bg-primary" data-bs-theme="dark">
+        
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">RStageENSAA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Connexion</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
     <div   style=" padding : 5% 30%"  data-bs-theme="dark">
     <?php
             if(isset($_POST['connecte'])){
