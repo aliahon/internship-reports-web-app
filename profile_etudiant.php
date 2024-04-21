ee59d7ccdff768c5ac08afe5fc1d790b1c1166e9
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-04-21 17:58:07 +0100

    template modified

--- profile_etudiant.php
+++ profile_etudiant.php
@@ -8,20 +8,18 @@
 </head>
 <body data-bs-theme="dark">
     <?php include 'include/nav.php'?>
-    <section style="background-color: #eee;" data-bs-theme="dark">
+    <section  data-bs-theme="dark">
         <div class="container py-5">
             <div class="row">
             <div class="col-lg-4">
                 <div class="card mb-4">
                 <div class="card-body text-center">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                    <img src="https://i.pinimg.com/236x/ad/73/1c/ad731cd0da0641bb16090f25778ef0fd.jpg" alt="avatar"
                     class="rounded-circle img-fluid" style="width: 150px;">
-                    <h5 class="my-3">John Smith</h5>
-                    <p class="text-muted mb-1">Full Stack Developer</p>
-                    <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                    <h5 class="my-3">N.E</h5>
+                    <p class="text-muted mb-1">Etudiant</p>
                     <div class="d-flex justify-content-center mb-2">
-                    <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary">Follow</button>
-                    <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary ms-1">Message</button>
+                    <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary">Déconnecter</button>
                     </div>
                 </div>
                 </div>
@@ -31,10 +29,10 @@
                 <div class="card-body">
                     <div class="row">
                     <div class="col-sm-3">
-                        <p class="mb-0">Full Name</p>
+                        <p class="mb-0">Le nom complet</p>
                     </div>
                     <div class="col-sm-9">
-                        <p class="text-muted mb-0">Johnatan Smith</p>
+                        <p class="text-muted mb-0">EL IDRISSI Nohaila<?php //l'extraction de nom et prenom sera faite ici ?></p>
                     </div>
                     </div>
                     <hr>
@@ -43,102 +41,25 @@
                         <p class="mb-0">Email</p>
                     </div>
                     <div class="col-sm-9">
-                        <p class="text-muted mb-0">example@example.com</p>
+                        <p class="text-muted mb-0">nohaila09el@gmail.com<?php //l'extraction de email sera faite ici ?></p>
                     </div>
                     </div>
                     <hr>
                     <div class="row">
                     <div class="col-sm-3">
-                        <p class="mb-0">Phone</p>
+                        <p class="mb-0">Filière</p>
                     </div>
                     <div class="col-sm-9">
-                        <p class="text-muted mb-0">(097) 234-5678</p>
+                        <p class="text-muted mb-0">Génie informatique<?php //l'extraction de Filière sera faite ici ?></p>
                     </div>
                     </div>
                     <hr>
                     <div class="row">
                     <div class="col-sm-3">
-                        <p class="mb-0">Mobile</p>
+                        <p class="mb-0">Niveau</p>
                     </div>
                     <div class="col-sm-9">
-                        <p class="text-muted mb-0">(098) 765-4321</p>
-                    </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                    <div class="col-sm-3">
-                        <p class="mb-0">Address</p>
-                    </div>
-                    <div class="col-sm-9">
-                        <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col-md-6">
-                    <div class="card mb-4 mb-md-0">
-                    <div class="card-body">
-                        <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status
-                        </p>
-                        <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                        <div class="progress rounded mb-2" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card mb-4 mb-md-0">
-                    <div class="card-body">
-                        <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status
-                        </p>
-                        <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                        <div class="progress rounded" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                        <div class="progress rounded mb-2" style="height: 5px;">
-                        <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
+                        <p class="text-muted mb-0"> 1ére anneé cycle d'ingénieur<?php //l'extraction de Niveau sera faite ici ?></p>
                     </div>
                     </div>
                 </div>
