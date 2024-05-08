71c42eb6de431385580e8dcbe1a61b878cb6ac34
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-05-08 22:49:04 +0100

    fixed the search bar

--- depotRapport.php
+++ depotRapport.php
@@ -6,12 +6,14 @@
     <title>Rapports</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body data-bs-theme="dark">
 <?php include 'include/nav.php'?>
     <?php include 'include/securité.php' ?>
 
-    <div class="container">
+    <div class="container" style=" padding : 2% 0%" >
     <?php
     // Vérifier si le formulaire a été soumis
     if (isset($_POST['Deposer'])) {
@@ -67,9 +69,19 @@
             </div>
             <!-- Ajoutez ici la sélection des étudiants qui ont contribué au rapport -->
             <!-- Exemple: -->
-            <div class="mb-3">
-                <label for="etudiants" class="form-label">Étudiants qui ont contribué au rapport</label>
-                <select multiple class="form-select" id="etudiants" name="etudiants[]" required>
+            <div class="mb-3 d-grid gap-2 d-md-block">
+                <div class="row align-items-center">
+                    <label for="etudiants" class="form-label col-md-4">Étudiants qui ont contribué au rapport</label>
+                    <div class="col-md-8">
+                        <div class="input-group">
+                            <input id="myInput" type="text" placeholder="Entrez le nom de l'étudiant rechercher..." class="form-control rounded" aria-label="Search" aria-describedby="search-addon" style="margin-bottom: 10px;" />
+                            <span class="input-group-text border-0" id="search-addon" style="margin-bottom: 10px;">
+                                <i class="fas fa-search"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <select id="select" multiple class="form-select" id="etudiants" name="etudiants[]" required>
                     <?php
                     require_once 'include/database.php';
                     // Requête pour récupérer la liste des étudiants depuis la base de données
@@ -77,22 +89,35 @@
                                                     FROM utilisateurs U
                                                     JOIN etudiant E ON U.ID_utilisateur = E.ID_utilisateur
                                                     JOIN filieres F ON E.ID_filiere = F.ID_filiere
-                                                    JOIN niveaux N ON E.ID_niveau = N.ID_niveau;')->fetchAll(PDO::FETCH_ASSOC);
+                                                    JOIN niveaux N ON E.ID_niveau = N.ID_niveau
+                                                    ORDER BY E.ID_filiere;')->fetchAll(PDO::FETCH_ASSOC);
 
                     foreach($etudiants as $etudiant){
 
                         ?>
-                        <option value="<?php echo $etudiant['ID_etudiant'] ?>"><?php echo  $etudiant['Nom'] . ' ' .  $etudiant['Prenom'] ?></option>
+                        <option  value="<?php echo $etudiant['ID_etudiant'] ?>"><?php echo  strtoupper($etudiant['Nom']) . ' ' .  ucwords($etudiant['Prenom'] )?></option>
                         <?php
                     }
                     ?>
                 </select>
             </div>
             <div class="col-12">
-            <button class="btn btn-primary" type="submit" name="Deposer">Déposer</button>
+            <button class="btn btn-primary" type="submit" name="Deposer">Déposer le rapport</button>
             </div>
         </form>
     </div>
+
+
+    <script>
+        $(document).ready(function(){
+        $("#myInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#select option").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+        });
+    </script>
 </body>
 </html>
 
