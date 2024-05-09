64301c8e105ba950e683b4d265d691a6974c2297
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-05-09 04:16:03 +0100

    some design touchs hhhhh

--- connexion.php
+++ connexion.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>connexion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Bootstrap CSS 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+     Font Awesome 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+     Custom CSS -->
 </head>
 </head>
 <body data-bs-theme="dark">
-<nav class="navbar  navbar-expand-lg  bg-primary" data-bs-theme="dark">
+    <nav class="navbar  navbar-expand-lg  bg-primary" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">RStageENSAA</a>
+            <a class="navbar-brand" href="index.php">StageENSAA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -23,8 +28,8 @@
             </div>
         </div>
     </nav>
-    <div   style=" padding : 5% 30%"  data-bs-theme="dark">
-    <?php
+    <div class="container" data-bs-theme="dark">
+        <?php
             if(isset($_POST['connecte'])){
                 $Email = $_POST['Email'];
                 $mdp = $_POST['Mot_de_passe'];
@@ -69,19 +74,39 @@
             }
 
         ?>
-        <h4>Connexion!</h4>
-        <form method="post">
-            <div class="mb-3" style="margin-top: 50px;">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" name ="Email">
+
+        <header class="py-4 text-center" style="padding:0 20%; margin:5% 0;">
+            <h1>Explorer les rapports de stageENSAA</h1>
+            <p>Découvrez notre plateforme pour accéder aux rapports de stage numérisés de l'École Nationale des Sciences Appliquées d'Agadir.</p>
+        </header>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Connectez-vous</h5>
+                            <form action="connexion.php" method="POST">
+                                <div class="mb-3">
+                                    <label for="email" class="form-label"></label>
+                                    <input type="email" class="form-control" id="email" name="Email" placeholder="Adresse e-mail" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label"></label>
+                                    <input type="password" class="form-control" id="password" name="Mot_de_passe" placeholder="Mot de passe" required>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary" name="connecte">Se connecter</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" name ="Mot de passe">
-            </div>
-            <button type="submit" class="btn btn-primary" name ="connecte">se connecte</button>
-        </form>
-    </div>
-    
+        </div>
+<!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,35 +1,138 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Explorer les rapports de stage de l'ENSAA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .navbar {
+            background-color: #343a40;
+        }
+
+        .about-us {
+            background-color: #212529;
+            padding: 50px 0;
+        }
+
+        .form-control {
+            background-color: #343a40;
+            border-color: #343a40;
+            color: #ffffff;
+        }
+
+        .form-control:focus {
+            background-color: #495057;
+            border-color: #495057;
+            color: #ffffff;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+        #hello {
+            background: url('img/1.png') no-repeat center top;
+            background-size: contain;
+            height: 110vh;
+        }
+
+        #intro {
+            background: url('img/2.png') no-repeat center top;
+            background-size: contain;
+            height: 100vh; 
+        }
+        
+        #goals {
+            background: url('img/3.png') no-repeat center top;
+            background-size: contain;
+            height: 100vh; 
+        }
+        
+        #fin {
+            background: url('img/4.png') no-repeat center top;
+            background-size: contain;
+            height: 110vh;
+        }
+
+        #finisio {
+            background: url('img/5.png') no-repeat center top;
+            background-size: contain;
+            height: 100vh;
+        }
+    </style>
 </head>
 <body data-bs-theme="dark">
-    <nav class="navbar  navbar-expand-lg  bg-primary" data-bs-theme="dark">
-        
-        <div class="container-fluid ">
-            <a class="navbar-brand" href="#">RStageENSAA</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-body-tertiary sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">StageENSAA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-                </li>
-            </ul>
-            <?php
-            if(isset($_POST['connecte1'])){
-                header('location: connexion.php');
-            }
-            ?>
-            <form class="d-flex" role="search" methode="post">
-                <button class="btn btn-outline-light" type="submit" name="connecte1" ><a class="nav-link" aria-current="page" href="connexion.php">se connecter</a></button>
-            </form>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#intro">Introduction</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#goals">Bénéfices</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contactez-nous</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#finisio">Contact Infos</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search" method="post">
+                    <button class="btn btn-outline-light" type="submit" name="connecte1"><a class="nav-link" aria-current="page" href="connexion.php">Se connecter</a></button>
+                </form>
             </div>
         </div>
     </nav>
+    
+    <div id="hello">
+    </div>
+
+    <div id="intro">
+    </div>
+
+    <div id="goals">
+    </div>
+
+    <div id="fin">
+    </div>
+    <div class="contact-form my-5" id="contact" style="padding:5%;">
+        <div class="container">
+            <h2 class="text-center">Contactez-nous</h2>
+            <form action="#" method="POST">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nom</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Adresse e-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div id="finisio">
+    </div>
 </body>
 </html>
